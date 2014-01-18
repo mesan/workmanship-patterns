085233e7df97602f788f8db573d8c66cd2c599bc
Lars Reed <lars.reed@mesan.no>
2014-01-18 23:01:17 +0100

    Felles time sheet entry

---
+++ src/test/java/no/mesan/fag/patterns/timesheet/TimesheetEntryTest.java
@@ -0,0 +1,31 @@
+package no.mesan.fag.patterns.timesheet;
+
+import no.mesan.fag.patterns.timesheet.TimesheetEntry;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test TimesheetEntry.
+ */
+public class TimesheetEntryTest {
+
+    @Test
+    public void shouldCreateEntriesFromStrings()  {
+        final List<TimesheetEntry> entries = TimesheetEntry.create(
+                "larsr", "2014-01-06", "1000", "30",
+                "larsr", "2014-01-06", "2134", "420",
+                "larsr", "2014-01-07", "2134", "540",
+                "larsr", "2014-01-07", "1001", "30",
+                "larsr", "2014-01-08", "2134", "450"
+        );
+        assertEquals(5, entries.size());
+        for (TimesheetEntry entry : entries)  {
+            assertEquals("larsr", entry.getUserID());
+            assertEquals(2014, entry.getWhen().year().get());
+        }
+        assertEquals(2134, entries.get(2).getActivity());
+    }
+}
