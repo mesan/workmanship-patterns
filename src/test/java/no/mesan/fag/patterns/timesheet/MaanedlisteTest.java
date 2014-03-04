5f7fa807b778ef14519db30ead43c84185fd7cfe
Lars Reed <lars.reed@mesan.no>
2014-03-04 23:31:32 +0100

    cleanup

--- src/test/java/no/mesan/fag/patterns/timesheet/MaanedlisteTest.java
+++ src/test/java/no/mesan/fag/patterns/timesheet/MaanedlisteTest.java
@@ -11,9 +11,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-/**
- * Test av Maanedliste.
- */
+/** Test av Maanedliste. */
 public class MaanedlisteTest {
 
     private Workbook wb;
