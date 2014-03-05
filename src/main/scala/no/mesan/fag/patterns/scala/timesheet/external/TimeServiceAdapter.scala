1006801010f12e7bfb726d267afaedce89c31896
Lars Reed <lars.reed@mesan.no>
2014-03-05 07:12:35 +0100

    "builder" i scala

---
+++ src/main/scala/no/mesan/fag/patterns/scala/timesheet/external/TimeServiceAdapter.scala
@@ -0,0 +1,22 @@
+package no.mesan.fag.patterns.scala.timesheet.external
+
+import no.mesan.fag.patterns.scala.timesheet.data.TimesheetEntry
+
+/** Adapter til den eksterne tjenesten. */
+trait TimeServiceAdapter {
+  /**
+   * Henter alle timene registrert for en gitt bruker.
+   *
+   * @param userID Brukeren vi vil hente timer for
+   * @return Liste av matchende timeføringer
+   */
+  def forEmployee(userID: String): Iterable[TimesheetEntry]
+
+  /**
+   * Henter alle timene registrert for et gitt år.
+   *
+   * @param year Året vi vil hente for
+   * @return Liste av matchende timeføringer
+   */
+  def forYear(year: Int): Iterable[TimesheetEntry]
+}
