6b8fa8c47ef9ddbda0f82f93ffe843ce684abd05
Lars Reed <lars.reed@mesan.no>
2014-03-05 22:58:18 +0100

    template method

--- src/main/scala/no/mesan/fag/patterns/scala/timesheet/Sheets.scala
+++ src/main/scala/no/mesan/fag/patterns/scala/timesheet/Sheets.scala
@@ -1,14 +1,137 @@
 package no.mesan.fag.patterns.scala.timesheet
 
-import no.mesan.fag.patterns.scala.timesheet.external.{TimeDataServer, TimeSource}
-import no.mesan.fag.patterns.scala.timesheet.data.TimesheetEntry
-import no.mesan.fag.patterns.scala.timesheet.format.StyleName
+import no.mesan.fag.patterns.scala.timesheet.external._
+import no.mesan.fag.patterns.scala.timesheet.data.{DoubleMatrix, TimesheetEntry}
+import no.mesan.fag.patterns.scala.timesheet.format._
 
 import java.io.FileOutputStream
 import org.apache.poi.ss.usermodel._
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 /** Superklasse for timelister. */
 abstract class Sheets {
+
+  /**
+   * Dette er hovedrutinen for å lage rapporter.
+   * Du må selvfølgelig overstyre alle abstrakte rutiner (! under), men du kan også gripe inn på andre nivåer --
+   * her er strukturen:
+   * <pre>
+   * dataRetrieve +  filter (funksjonsarg)
+   *   retrieve!
+   * dataGroup
+   *    dataExtraHeadings -- evt. tilleggsoverskrifter, default ingen
+   *    colRow !
+   * createWorkbook
+   * createHeading
+   *    headingTexts!
+   * createTableHead
+   * createDataGrid
+   * createSums
+   * finish
+   * </pre>
+   *
+   * @param dataService Tjeneste for å hente data
+   * @param title Navn på arbeidsboken
+   * @param headTitle Øverste venstre overskrift i tabellen
+   * @param sortedCols Om kolonneoverskriftene skal være sortert
+   * @return Resultat
+   */
+  protected final def generateReport(dataService: TimeDataService, title: String, headTitle: String,
+                                     sortedCols: Boolean = true) (filter: TimesheetEntry=>Boolean) : Workbook = {
+    val list = dataRetrieve(dataService, filter)
+    val matrix = dataGroup(list)
+    val sheet = createWorkbook(title)
+    val workbook = sheet.getWorkbook
+    val styles = StyleFactory.styleSetup(workbook)
+    createHeading(sheet, styles)
+    createTableHead(sheet, matrix, styles, headTitle, sortedCols)
+    createDataGrid(sheet, matrix, styles, sortedCols)
+    createSums(sheet, matrix, styles)
+    finish(sheet, matrix.cSize)
+    workbook
+  }
+
+  protected def dataRetrieve(service: TimeDataService, filterFun: TimesheetEntry=>Boolean): List[TimesheetEntry]=
+    retrieve(new TimeIteratorService(service)).toList filter filterFun
+  def retrieve(service: TimeIteratorService): Iterable[TimesheetEntry]
+
+  protected def dataGroup(entries: List[TimesheetEntry]): DoubleMatrix = {
+    val matrix= new DoubleMatrix
+    dataExtraHeadings(matrix)
+    for (entry <- entries) {
+      val (colRef, rowRef) = colRow(entry)
+      matrix.add(colRef, rowRef, minutesToHours(entry))
+    }
+    matrix
+  }
+  protected def dataExtraHeadings(matrix: DoubleMatrix): Unit = Unit
+  def colRow(entry: TimesheetEntry): (String, String)
+
+  protected def createWorkbook(title: String): Sheet = {
+    val sheet= (new XSSFWorkbook).createSheet(title)
+    sheet.getPrintSetup.setLandscape(true)
+    sheet.setFitToPage(true)
+    sheet.setHorizontallyCenter(true)
+    sheet
+  }
+
+  protected def createHeading(sheet: Sheet, styles: Map[StyleName, CellStyle]) {
+    var colnum= 0
+    val heading1 = createRow(sheet, 0, 45)
+    for (s<-headingTexts()) colnum= makeCell(heading1, colnum, H1, styles) { cell:Cell => cell.setCellValue(s)}
+  }
+  def headingTexts(): List[String]
+
+  protected def createTableHead(sheet: Sheet, matrix: DoubleMatrix, styles: Map[StyleName, CellStyle],
+                                title: String, sortedCols: Boolean) {
+    val tableHead = createRow(sheet, 2, 40)
+    var colnum = 0
+    for (header <- Vector(title, "Sum") ++ matrix.colKeys(sortedCols))
+      colnum= makeCell(tableHead, colnum, if (colnum < 2) TableHeadLeft else TableHead, styles) {
+        cell=> cell.setCellValue(header)
+      }
+  }
+
+  protected def createDataGrid(sheet: Sheet, matrix: DoubleMatrix, styles: Map[StyleName, CellStyle], sortedCols: Boolean) {
+    var rownum= 3
+    for (rKey <- matrix.rowKeys(sortedCols)) {
+      var colnum= 0
+      val row= createRow(sheet, rownum)
+      rownum +=1
+      colnum= makeCell(row, colnum, Col1, styles) { cell:Cell => cell.setCellValue(rKey)}
+      // Sum
+      val ref = cellRef(colnum+2, rownum) + ":" + cellRef(matrix.cSize + 2, rownum)
+      colnum= makeCell(row, colnum, ColN, styles) { cell:Cell => cell.setCellFormula("SUM(" + ref + ")")}
+      // Data
+      for (c <- matrix.colKeys(sorted=true))
+        colnum= makeCell(row, colnum, Data, styles) { cell:Cell => matrix.get(c, rKey) map cell.setCellValue }
+    }
+  }
+
+  protected def createSums(sheet: Sheet, matrix: DoubleMatrix, styles: Map[StyleName, CellStyle]) {
+    var colnum = 0
+    val rownum = sheet.getLastRowNum+1
+    val row = createRow(sheet,rownum)
+    colnum= makeCell(row, colnum, Sum1, styles) { cell:Cell => cell.setCellValue("SUM")}
+    for (i <- 1 to matrix.cSize+1) {
+      val ref = cellRef(i + 1, 4) + ":" + cellRef(i + 1, rownum)
+      colnum= makeCell(row, colnum, Sums, styles) { cell:Cell => cell.setCellFormula("SUM(" + ref + ")")}
+    }
+  }
+
+  protected def finish(sheet: Sheet, cols: Int) {
+    import scala.collection.JavaConversions._
+    // Rekalkuler
+    val evaluator= sheet.getWorkbook.getCreationHelper.createFormulaEvaluator
+    for (r: Row <- sheet.rowIterator();
+         c: Cell <- r)
+      if (c.getCellType == Cell.CELL_TYPE_FORMULA) evaluator.evaluateFormulaCell(c)
+    for (i <- 0 to cols) {
+      sheet.autoSizeColumn(i)
+      sheet.setColumnWidth(i, (1.05 * sheet.getColumnWidth(i)).asInstanceOf[Int])
+    }
+  }
+
   /**
    * Lag en cellereferanse (type A2B5) for en gitt kolonne+rad.
    * @param col Kolonne
@@ -53,10 +176,13 @@ object Sheets extends App {
   val timeliste = new Timeliste("larsr", 2014, 2, source)
   val wb1 = timeliste.createTimeliste
   timeliste.writeToFile("Timeliste-scala", wb1)
-  val mndListe: Maanedliste = new Maanedliste(2014, 2, source)
-  val wb2: Workbook = mndListe.createMaanedliste
+  val mndListe= new Maanedliste(2014, 2, source)
+  val wb2 = mndListe.createMaanedliste
   mndListe.writeToFile("Månedsoppgjør-scala", wb2)
-  val aarsListe: Aarsliste = new Aarsliste(2014, source)
-  val wb3: Workbook = aarsListe.createAarsoversikt
+  val aarsListe= new Aarsliste(2014, source)
+  val wb3 = aarsListe.createAarsoversikt
   aarsListe.writeToFile("Årsoversikt-scala", wb3)
+  val ukeListe = new Ukeliste(2014, 1, 15, source)
+  val wb4 = ukeListe.createUkeliste
+  ukeListe.writeToFile("Ukeoversikt-scala", wb4)
 }
---
+++ src/main/scala/no/mesan/fag/patterns/scala/timesheet/Ukeliste.scala
@@ -0,0 +1,34 @@
+package no.mesan.fag.patterns.scala.timesheet
+
+import no.mesan.fag.patterns.scala.timesheet.external.{TimeIteratorService, TimeDataService}
+import no.mesan.fag.patterns.scala.timesheet.data.{DoubleMatrix, TimesheetEntry}
+
+import org.apache.poi.ss.usermodel.Workbook
+import org.joda.time.{DateTimeConstants, LocalDate}
+
+/** Timeliste for alle brukere for en enkelt uke. */
+class Ukeliste(year: Int, month: Int, day: Int, source: TimeDataService) extends Sheets {
+  private val Days = Array("Man", "Tir", "Ons", "Tor", "Fre", "Lør", "Søn")
+  private val (fromDate, toDate) = {
+    val date= new LocalDate(year, month, day)
+    (date.withDayOfWeek(DateTimeConstants.MONDAY),date.withDayOfWeek(DateTimeConstants.SUNDAY))
+  }
+
+  def createUkeliste: Workbook =
+    generateReport(source, Ukeliste.SheetTitle, "Aktivitet/bruker -- dag", sortedCols = false) { entry =>
+      !(entry.when.isBefore(fromDate) || entry.when.isAfter(toDate))
+    }
+
+  override def retrieve(service: TimeIteratorService): Iterable[TimesheetEntry] =  service.forYear(year)
+
+  override def dataExtraHeadings(matrix: DoubleMatrix) = for (day <-Days) matrix.ensureCol(day)
+
+  override def colRow(entry: TimesheetEntry): (String, String)=
+    (Days(entry.when.getDayOfWeek-1), s"${entry.activity} / ${entry.userID}")
+
+  override def headingTexts() = List(Ukeliste.SheetTitle, fromDate.toString("yyyyMMdd"),"-",toDate.toString("yyyyMMdd"))
+}
+
+object Ukeliste {
+  var SheetTitle= "Ukeliste"
+}
