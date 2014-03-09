aa8ed14867e862bdecabeaaf13848c3e24e08081
Lars Reed <lars.reed@mesan.no>
2014-03-09 15:00:15 +0100

    implicit adapter

--- src/main/scala/no/mesan/fag/patterns/scala/timesheet/Timeliste.scala
+++ src/main/scala/no/mesan/fag/patterns/scala/timesheet/Timeliste.scala
@@ -1,6 +1,6 @@
 package no.mesan.fag.patterns.scala.timesheet
 
-import no.mesan.fag.patterns.scala.timesheet.external.{TimeIteratorService, TimeDataService}
+import no.mesan.fag.patterns.scala.timesheet.external.TimeDataService
 import no.mesan.fag.patterns.scala.timesheet.data.{TimesheetEntry, DoubleMatrix}
 
 import org.apache.poi.ss.usermodel._
@@ -15,7 +15,7 @@ class Timeliste(user: String, year: Int, month: Int, source: TimeDataService) ex
       entry.when.year.get == year && entry.when.monthOfYear.get == month
     }
 
-  override def retrieve(service: TimeIteratorService): Iterable[TimesheetEntry] =  service.forEmployee(user)
+  override def retrieve(service: TimeDataService): Iterable[TimesheetEntry] =  service.forEmployee(user)
 
   override def dataExtraHeadings(matrix: DoubleMatrix) {
     for (i<- 1 until new LocalDate(year, month, 1).dayOfMonth.getMaximumValue) matrix.ensureCol(dayRef(i))
--- src/main/scala/no/mesan/fag/patterns/scala/timesheet/external/TimeServiceAdapter.scala
+++ src/main/scala/no/mesan/fag/patterns/scala/timesheet/external/TimeServiceAdapter.scala
@@ -21,3 +21,38 @@ trait TimeServiceAdapter {
    */
   def forYear(year: Int): Iterable[TimesheetEntry]
 }
+
+class TimeServiceIterator(source: TimeDataService) extends TimeServiceAdapter {
+  private class EntryIterator(fillFunction: Int=>List[TimesheetEntry]) extends Iterator[TimesheetEntry] {
+    var buffer: List[TimesheetEntry] = Nil
+    var fetched = 0
+    var lastIsFetched = false
+
+    override def hasNext: Boolean = {
+      refillIfNeeded
+      !buffer.isEmpty
+    }
+
+    private def refillIfNeeded {
+      if (!buffer.isEmpty || lastIsFetched) return
+      val newBatch: List[TimesheetEntry] = fillFunction(fetched)
+      fetched += newBatch.size
+      buffer ++= newBatch
+      lastIsFetched = newBatch.isEmpty
+    }
+
+    override def next(): TimesheetEntry = {
+      refillIfNeeded
+      if (buffer.isEmpty) throw new NoSuchElementException
+      val entry= buffer.head
+      buffer= buffer.tail
+      entry
+    }
+  }
+
+  override def forEmployee(userID: String): Iterable[TimesheetEntry] =
+    new EntryIterator(fetched => source.forEmployee(userID, fetched)).toIterable
+
+  override def forYear(year: Int): Iterable[TimesheetEntry]=
+    new EntryIterator(fetched => source.forYear(year, fetched)).toIterable
+}
